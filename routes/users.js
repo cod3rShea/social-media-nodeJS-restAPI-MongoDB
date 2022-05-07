24e0a26fc3a66be573016aef9d7946be08911f62
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-07 13:13:58 -0400

    get user and pass back data... remove password and updatedAt from data set

--- routes/users.js
+++ routes/users.js
@@ -41,6 +41,15 @@ router.delete("/:id", async (req, res) => {
 	}
 });
 //get a user
+router.get("/:id", async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const { password, updatedAt, ...other } = user._doc;
+		res.status(200).json(other);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 //follow a user
 //unfollow a user
 //description
