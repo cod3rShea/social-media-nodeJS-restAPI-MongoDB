a0304faa6b17bbc79fd6b1598148efa4f19f92e3
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-06 21:28:11 -0400

    add route for auth

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, () => {
@@ -14,9 +16,9 @@ mongoose.connect(process.env.MONGO_URL, () => {
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
-const userRoute = require("./routes/users");
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
 	console.log("Backend server is running");
