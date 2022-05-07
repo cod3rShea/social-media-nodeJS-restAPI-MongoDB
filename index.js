bc0f4947ebe753e639c9cf602a76f8852138c531
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-06 21:06:25 -0400

    connect db

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL, () => {
+	console.log("conected to server");
+});
 
 app.listen(8800, () => {
 	console.log("Backend server is running");
