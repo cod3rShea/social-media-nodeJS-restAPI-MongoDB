3a245ee9daf15e3a169f3c20616fe25f7ce0365b
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-07 12:08:09 -0400

    add logic for deleting users

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt");
 
 //update user
 router.put("/:id", async (req, res) => {
+	// checks to see if user ID matches or is admin
 	if (req.body.userId === req.params.id || req.body.isAdmin) {
 		if (req.body.password) {
 			try {
@@ -26,6 +27,19 @@ router.put("/:id", async (req, res) => {
 	}
 });
 // delete user
+//delete user
+router.delete("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			await User.findByIdAndDelete(req.params.id);
+			res.status(200).json("Account has been deleted");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("You can delete only your account!");
+	}
+});
 //get a user
 //follow a user
 //unfollow a user
