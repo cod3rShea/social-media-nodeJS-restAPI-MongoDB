fbec4f792b026b17c59fe2a6257833bfc90f9e3f
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-07 11:27:06 -0400

    Add logic to make email lowercase

--- routes/auth.js
+++ routes/auth.js
@@ -2,25 +2,46 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-//Register
+//REGISTER
 router.post("/register", async (req, res) => {
 	try {
-		// encyrpt password
+		//generate new password
 		const salt = await bcrypt.genSalt(10);
 		const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
+		const emailLowerCase = await req.body.email.toLowerCase();
 		//create new user
-		const newUser = await new User({
+		const newUser = new User({
 			username: req.body.username,
-			email: req.body.email,
+			email: emailLowerCase,
 			password: hashedPassword,
 		});
 
-		//save user and return response
+		//save user and respond
 		const user = await newUser.save();
 		res.status(200).json(user);
-	} catch (error) {
-		console.log("is this my error", error);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+	try {
+		const user = await User.findOne({
+			email: req.body.email.toLowerCase(),
+		});
+
+		!user && res.status(404).json("user not found");
+
+		const validPassword = await bcrypt.compare(
+			req.body.password,
+			user.password
+		);
+		!validPassword && res.status(400).json("wrong password");
+
+		res.status(200).json(user);
+	} catch (err) {
+		res.status(500).json(err);
 	}
 });
 
