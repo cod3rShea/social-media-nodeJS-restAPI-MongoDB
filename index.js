5bd96f01c4d5fc29381f24d91ceedd9e6f5c7f80
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-06 21:17:06 -0400

    add middleware and test sending content to index

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ mongoose.connect(process.env.MONGO_URL, () => {
 	console.log("conected to server");
 });
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+	res.send("welcome to home page");
+});
+
+app.get("/users", (req, res) => {
+	res.send("welcome to users");
+});
+
 app.listen(8800, () => {
 	console.log("Backend server is running");
 });
