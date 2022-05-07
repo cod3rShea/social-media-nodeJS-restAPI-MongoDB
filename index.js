9510639a1202c161eaf2a91f7e3895e0e537314a
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-06 20:41:05 -0400

    add other libraries and create env file

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
 
 app.listen(8800, () => {
 	console.log("Backend server is running");
