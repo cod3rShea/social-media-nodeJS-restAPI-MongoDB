e2744753820b7de48fee7aade74a56eee58435a8
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-07 11:54:08 -0400

    add logic to update user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,35 @@
+const User = require("../models/User");
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
+//update user
+router.put("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		if (req.body.password) {
+			try {
+				const salt = await bcrypt.genSalt(10);
+				req.body.password = await bcrypt.hash(req.body.password, salt);
+			} catch (err) {
+				return res.status(500).json(err);
+			}
+		}
+		try {
+			const user = await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body,
+			});
+			res.status(200).json("Account has been updated");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("You can update only your account!");
+	}
+});
+// delete user
+//get a user
+//follow a user
+//unfollow a user
+//description
 router.get("/", (req, res) => {
 	res.send("test user route");
 });
