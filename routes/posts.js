cdfa0da125a5b935fc3fd764032d84fb2a195dc1
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-08 09:06:43 -0400

    add creating post logic

--- routes/posts.js
+++ routes/posts.js
@@ -1,2 +1,15 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
+const User = require("../models/User");
+
+//create a post
+router.post("/", async (req, res) => {
+	const newPost = new Post(req.body);
+	try {
+		const savedPost = await newPost.save();
+		res.status(200).json(savedPost);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 module.exports = router;
