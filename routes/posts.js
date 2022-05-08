31e3777052bfd08a7731f9ab719bfe0e9783e469
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-08 10:47:47 -0400

    logic for updating post

--- routes/posts.js
+++ routes/posts.js
@@ -13,3 +13,18 @@ router.post("/", async (req, res) => {
 	}
 });
 module.exports = router;
+
+//update post
+router.put("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (post.userId === req.body.userId) {
+			await post.updateOne({ $set: req.body });
+			res.status(403).json("your post has been updated");
+		} else {
+			res.status(403).json("you can only update your own post");
+		}
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
