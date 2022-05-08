39c45a2668424a882043409a64a3bbcd5d2e51a5
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-08 11:20:10 -0400

    get single post logic

--- routes/posts.js
+++ routes/posts.js
@@ -58,3 +58,15 @@ router.put("/:id/like", async (req, res) => {
 		res.status(500).json(err);
 	}
 });
+
+// get post
+router.get("/:id", async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
