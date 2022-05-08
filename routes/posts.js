db67db156751018d3457cb043780ef9d28eb19aa
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-08 11:31:57 -0400

    get timeline post

--- routes/posts.js
+++ routes/posts.js
@@ -69,4 +69,20 @@ router.get("/:id", async (req, res) => {
 	}
 });
 
+//get timeline posts
+router.get("/timeline/all", async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.body.userId);
+		const userPosts = await Post.find({ userId: currentUser._id });
+		const friendPosts = await Promise.all(
+			currentUser.followings.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+		res.json(userPosts.concat(...friendPosts));
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
