ac13b65c96260d588cecbbee932b72446df0e87b
Sean Michael Shea <seanshea1337@gmail.com>
2022-05-08 08:13:18 -0400

    add logic to follow and unfollow

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,7 @@ router.put("/:id", async (req, res) => {
 		return res.status(403).json("You can update only your account!");
 	}
 });
-// delete user
+
 //delete user
 router.delete("/:id", async (req, res) => {
 	if (req.body.userId === req.params.id || req.body.isAdmin) {
@@ -44,6 +44,7 @@ router.delete("/:id", async (req, res) => {
 router.get("/:id", async (req, res) => {
 	try {
 		const user = await User.findById(req.params.id);
+		// removes password and updatedAt from data
 		const { password, updatedAt, ...other } = user._doc;
 		res.status(200).json(other);
 	} catch (err) {
@@ -51,10 +52,49 @@ router.get("/:id", async (req, res) => {
 	}
 });
 //follow a user
+router.put("/:id/follow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+			if (!user.followers.includes(req.body.userId)) {
+				await user.updateOne({ $push: { followers: req.body.userId } });
+				await currentUser.updateOne({
+					$push: { followings: req.params.id },
+				});
+				res.status(200).json("user has been followed");
+			} else {
+				res.status(403).json("you allready follow this user");
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	} else {
+		res.status(403).json("you cant follow yourself");
+	}
+});
+
 //unfollow a user
-//description
-router.get("/", (req, res) => {
-	res.send("test user route");
+router.put("/:id/unfollow", async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+			if (user.followers.includes(req.body.userId)) {
+				await user.updateOne({ $pull: { followers: req.body.userId } });
+				await currentUser.updateOne({
+					$pull: { followings: req.params.id },
+				});
+				res.status(200).json("user has been unfollowed");
+			} else {
+				res.status(403).json("you dont follow this user");
+			}
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	} else {
+		res.status(403).json("you cant unfollow yourself");
+	}
 });
 
 module.exports = router;
